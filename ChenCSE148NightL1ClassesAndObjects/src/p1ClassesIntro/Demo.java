d3dee13ee0b8c18bdb60ef038b46dedf29338438
Chen <chenb@sunysuffolk.edu>
2018-09-06 19:55:08 -0400

    Classes and Objects

--- ChenCSE148NightL1ClassesAndObjects/src/p1ClassesIntro/Demo.java
+++ ChenCSE148NightL1ClassesAndObjects/src/p1ClassesIntro/Demo.java
@@ -4,20 +4,27 @@ public class Demo {
 
 	public static void main(String[] args) {
 		Student s1 = new Student();
-		s1.age = 19;
-		s1.gpa = 4.0;
-		s1.name = "John";
-		s1.id = "1234567";
-		s1.driveToSchool();
-		s1.takeTheBus();
+//		s1.setName("John");
+//		s1.setAge(20);
+//		s1.setGpa(4.0);
+//		s1.setId("1111");
+//		s1.setAll("John", "1111", 21, 3.0);
+		System.out.println(s1);
 		
-		Student s2 = new Student();
-		s2.age = 20;
-		s2.gpa = 2.0;
-		s2.name = "Jane";
-		s2.id = "7654321";
-		s2.driveToSchool();
-		s2.takeTheBus();
+		// create another object
+		Student s2 = new Student("Jane", "3333", 20, 4.0);
+		System.out.println(s2);
+//		System.out.println(s1.getName());
+//		System.out.println(s1.getId());
+//		System.out.println(s1.getAge());
+//		System.out.println(s1.getGpa());
+		
+//		s1.driveToSchool();
+//		s1.takeTheBus();
+//		
+//		Student s2 = new Student();
+//		s2.driveToSchool();
+//		s2.takeTheBus();
 	}
 
 }
---
+++ ChenCSE148NightL2ClassesAndObject 2/src/p1Car/Demo.java
@@ -0,0 +1,22 @@
+package p1Car;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Car car1 = new Car();
+		System.out.println(car1);
+		
+		Car car2 = new Car("Honda", 2018, 20200.95, "RED");
+		System.out.println(car2);
+		car2.setColor("WHITE");
+		System.out.println(car2);
+		System.out.println("The year of the second car is: " + car2.getYear());
+		System.out.println("The speed is " + car2.getSpeed());
+		car2.speedUp(30);
+		car2.speedUp(40);
+		System.out.println("The speed is " + car2.getSpeed());
+		car2.slowDown(50);
+		System.out.println("The speed is " + car2.getSpeed());
+	}
+
+}
