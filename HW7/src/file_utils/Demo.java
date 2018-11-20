752657a0bfc4e661efe9af8799fbd918c9409f03
Chen <chenb@sunysuffolk.edu>
2018-11-20 15:33:39 -0500

    Bag Review

--- HW7/src/file_utils/Demo.java
+++ HW7/src/file_utils/Demo.java
@@ -3,8 +3,8 @@ package file_utils;
 public class Demo {
 
 	public static void main(String[] args) {
-			FileFactory.makeStudentFile(10, "outputData/Students.txt");
-			FileFactory.makeTextbookFile(3, "outputData/Textbooks.txt");
+			FileFactory.makeStudentFile(100, "outputData/Students.txt");
+			FileFactory.makeTextbookFile(30, "outputData/Textbooks.txt");
 	}
 
 }
